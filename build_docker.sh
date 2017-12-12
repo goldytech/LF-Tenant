33df3c3e80074933c6067e52e879ec730f3378b4
Muhammad Afzal Qureshi <afzal.qureshi@outlook.com>
2017-12-12 16:07:12 +0530

    testing

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,6 @@
+#! /bin/bash
 GITSHA1=${GITSHA1:=$(git rev-parse HEAD)}
-DOCKER_IMAGE_VERSION=${BUILD_NUMBER}.${GITSHA1}
-
+DOCKER_IMAGE_VERSION=${GITSHA1:1:5}
+echo $DOCKER_IMAGE_VERSION
 docker build -t goldytech/lf-tenant2:$DOCKER_IMAGE_VERSION .
 
